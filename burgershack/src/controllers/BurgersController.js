7942760d87fb1503f8989b96b5be11a5e6dbcf9a
ethanrgibson <ethanrgibson33@gmail.com>
2025-03-10 14:07:18 -0600

    init and burger controller made

---
+++ burgershack/src/controllers/BurgersController.js
@@ -0,0 +1,29 @@
+import BaseController from "../utils/BaseController.js"
+
+export class BurgersController extends BaseController {
+
+  constructor() {
+
+    super('api/burgers')
+
+    this.router
+      .get('/test', this.getTest)
+
+  }
+
+  async getTest(request, response, next) {
+
+    try {
+
+      response.send('burger test success')
+
+
+    } catch (error) {
+      next(error)
+
+    }
+
+
+  }
+
+}
